927ab6431ca8870b7b2bb8c13fec9b14b6b39aff
Effendi Nugraha <effendi94.autobot@gmail.com>
2025-06-30 15:25:13 +0700

    fix scroll

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -20,28 +20,28 @@ class HomeView extends StackedView<HomeViewmodel> {
   @override
   Widget builder(BuildContext context, HomeViewmodel viewModel, Widget? child) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            spacing: 20,
-            children: [
-              Container(
-                width: screenWidth(context),
-                decoration: BoxDecoration(
-                  color: CustomColors.blue50,
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    children: [Text('Welcome Trader'), Text('Make your investment today')],
-                  ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 20,
+          children: [
+            Container(
+              width: screenWidth(context),
+              decoration: BoxDecoration(
+                color: CustomColors.blue50,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [Text('Welcome Trader'), Text('Make your investment today')],
                 ),
               ),
-              Text('List Coins', style: Theme.of(context).textTheme.labelLarge),
-              ListView.separated(
+            ),
+            Text('List Coins', style: Theme.of(context).textTheme.labelLarge),
+            Expanded(
+              child: ListView.separated(
                 shrinkWrap: true,
                 itemCount: viewModel.listAssets.length,
                 separatorBuilder: (context, index) => const SizedBox(height: 8),
@@ -73,8 +73,8 @@ class HomeView extends StackedView<HomeViewmodel> {
                   );
                 },
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
