41ac97b0f52fc3c31cded7b7ae86ce8ea0424a34
Effendi Nugraha <effendi94.autobot@gmail.com>
2025-06-30 16:49:37 +0700

    update profile view & viewmodel

--- lib/ui/views/profile/profile_view.dart
+++ lib/ui/views/profile/profile_view.dart
@@ -1,13 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:trading_sample_app/app/constants/custom_colors.dart';
+import 'package:trading_sample_app/ui/shared/circle_avatar_widget.dart';
 import 'package:trading_sample_app/ui/views/profile/profile_viewmodel.dart';
 
 class ProfileView extends StackedView<ProfileViewmodel> {
   const ProfileView({super.key});
 
   @override
+  void onViewModelReady(ProfileViewmodel viewModel) {
+    viewModel.init();
+    super.onViewModelReady(viewModel);
+  }
+
+  @override
   Widget builder(BuildContext context, viewModel, Widget? child) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          spacing: 20,
+          children: [
+            Card(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+              color: CustomColors.brandBlueSecondary,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 30),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                    // crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 5,
+                    children: [
+                      CircleAvatarWidget(radius: 50, assetPath: 'assets/images/avatar.png'),
+                      Text(
+                        '${viewModel.profile?.name}',
+                        style: Theme.of(
+                          context,
+                        ).textTheme.labelLarge?.copyWith(color: CustomColors.neutralWhite),
+                      ),
+                      Text(
+                        '${viewModel.profile?.email}',
+                        style: Theme.of(
+                          context,
+                        ).textTheme.labelLarge?.copyWith(color: CustomColors.neutralWhite),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.separated(
+                itemCount: viewModel.listProfileNav.length,
+                separatorBuilder: (_, __) => const SizedBox(height: 15),
+                itemBuilder: (context, index) {
+                  final item = viewModel.listProfileNav[index];
+                  return ListTile(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                    tileColor: CustomColors.neutral40,
+                    leading: Icon(item['icon']),
+                    title: Text(item['title']),
+                    subtitle: item['subtitle'] != null ? Text(item['subtitle']) : null,
+                    trailing: const Icon(Icons.chevron_right),
+                    onTap: item['onTap'] as void Function(),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
--- lib/ui/views/profile/profile_viewmodel.dart
+++ lib/ui/views/profile/profile_viewmodel.dart
@@ -1,3 +1,49 @@
+import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:trading_sample_app/app/app.locator.dart';
+import 'package:trading_sample_app/app/enums/snackbar_type.dart';
+import 'package:trading_sample_app/app/models/profile_model.dart';
+import 'package:trading_sample_app/app/services/order_service.dart';
+import 'package:trading_sample_app/app/services/profile_service.dart';
 
-class ProfileViewmodel extends BaseViewModel {}
+class ProfileViewmodel extends BaseViewModel {
+  final ProfileService _profileService = locator<ProfileService>();
+  final SnackbarService _snackbarService = locator<SnackbarService>();
+  final OrderService _orderService = locator<OrderService>();
+
+  ProfileModel? get profile => _profileService.profile;
+
+  List<Map<String, dynamic>> listProfileNav = [];
+
+  void init() {
+    listProfileNav = [
+      {'icon': Icons.account_balance, 'title': 'Bank Details', 'onTap': () => onNavClick()},
+      {'icon': Icons.security, 'title': 'Security', 'onTap': () => onNavClick()},
+      {'icon': Icons.help, 'title': 'Help and Support', 'onTap': () => onNavClick()},
+      {'icon': Icons.policy, 'title': 'Terms and Conditions', 'onTap': () => onNavClick()},
+      {
+        'icon': Icons.delete,
+        'title': 'Reset Data',
+        'subtitle': 'Reset all trading data (orders, balance) for testing purpose (dev)',
+        'onTap': () => resetAllData(),
+      },
+    ];
+  }
+
+  void resetAllData() {
+    _orderService.clearOrders();
+    _profileService.resetBalance();
+    _snackbarService.showCustomSnackBar(
+      message: 'All data has been successfully reset',
+      variant: SnackbarType.info,
+    );
+  }
+
+  void onNavClick() {
+    if (_snackbarService.isSnackbarOpen) {
+      _snackbarService.closeSnackbar();
+    }
+    _snackbarService.showCustomSnackBar(message: 'Comming Soon!', variant: SnackbarType.info);
+  }
+}
